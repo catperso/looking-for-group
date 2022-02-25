78d1865d04eddd593680265f76c9ce4a7980ab47
Matt Casperson <mc.casperson@gmail.com>
2022-02-25 14:39:53 -0800

    add placeholder for GameDetail.js

---
+++ src/components/GameDetail.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+function GameDetail(props){
+  const { game, onClickingDelete } = props;
+
+  return (
+    <React.Fragment>
+      <h1>Game Detail goes Here!</h1>
+    </React.Fragment>
+  );
+}
+
+GameDetail.propTypes = {
+  game: PropTypes.object,
+  onClickingDelete: PropTypes.func
+}
+
+export default GameDetail;
