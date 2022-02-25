371f2f4b1409c381064b411c7a835ca31202b4c0
Matt Casperson <mc.casperson@gmail.com>
2022-02-25 14:36:34 -0800

    add placeholder for GameList.js; force install redux and react-redux

---
+++ src/components/GameList.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Game from './Game';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase'
+
+function GameList(props){
+  useFirestoreConnect([ { collection: 'games' } ]);
+  const games = useSelector(state => state.firestore.ordered.games);
+
+  if (isEmpty(games)) {
+    return(
+      <React.Fragment>
+        <h3>No games to display!</h3>
+      </React.Fragment>
+    );
+  }
+
+  if (isLoaded(games)) {
+    return(
+      <React.Fragment>
+        <hr/>
+        {games.map((game) => {
+          return <Game 
+            whenGameClicked = {props.onGameSelection}
+            id = {game.id}
+            key = {game.id}
+          />
+        })}
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <h3>Games are loading...</h3>
+      </React.Fragment>
+    );
+  }
+}
+
+GameList.propTypes = {
+  onGameSelection: PropTypes.func
+}
+
+export default GameList;
