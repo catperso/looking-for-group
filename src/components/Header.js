c0db7e868d9e6204c955e5c6b55d3808eaefc9bc
Matt Casperson <mc.casperson@gmail.com>
2022-03-04 10:45:38 -0800

    correct firebase.js config, update button routing in header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header(){
       <h1>Looking For Group</h1>
       <h2>DND Session Coordinator</h2>
       <button className="btn btn-info"><Link to='/'>Games List</Link></button>
-      <button className="btn btn-warning"><Link to='/account'>Account Details</Link></button>
+      <button className="btn btn-warning"><Link to='/signin'>Account Details</Link></button>
     </React.Fragment>
   );
 }
