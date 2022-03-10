97d0b2eb6a4a5fd286ae370111452667ee060853
Matt Casperson <mc.casperson@gmail.com>
2022-03-10 10:23:39 -0800

    add header logo image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import dungindragons from "./../img/dungin-dragons.png";
 
 function Header(){
   return (
     <React.Fragment>
+      <img src={dungindragons} alt='a logo' className="dunginLogo" />
       <h1>Looking For Group</h1>
       <h2>DND Session Coordinator</h2>
       <NavLink to='/' className="btn btn-info">Games List</NavLink>
