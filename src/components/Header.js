0888ba37d640d976244e6aaca5cafea251c3d0c5
Matt Casperson <mc.casperson@gmail.com>
2022-03-10 13:11:25 -0800

    add styling to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,14 @@ import dungindragons from "./../img/dungin-dragons.png";
 function Header(){
   return (
     <React.Fragment>
+      <div className="headerZone">
       <img src={dungindragons} alt='a logo' className="dunginLogo" />
       <h1>Looking For Group</h1>
       <h2>DND Session Coordinator</h2>
       <NavLink to='/' className="btn btn-info">Games List</NavLink>
       <NavLink to='/signin' className="btn btn-warning" activeClassName="disabled">Sign In/Up/Out</NavLink>
       <hr/>
+      </div>
     </React.Fragment>
   );
 }
