58bdd27db82246001c28c455212837d79a2dee6d
Matt Casperson <mc.casperson@gmail.com>
2022-03-14 11:02:44 -0700

    add background image; adjust game list styling

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,13 +4,12 @@ import PropTypes from 'prop-types';
 function Game(props){
   return(
     <React.Fragment>
-      <div onClick={() => props.whenGameClicked(props.id)}>
+      <div onClick={() => props.whenGameClicked(props.id)} className="gameListGameDiv">
         <h2>{props.gameName}</h2>
         <h3>DM - <em>{props.gameHost}</em></h3>
         <p>{props.gamePlayers.length}/{props.gameMaxPlayers} players</p>
         <p className='gameDetailTags'>{props.gameTags.join(' ') || "no tags here"}</p>
         <p className='gameDetailTags'>Game scheduled for: {props.gameTime}</p>
-        <hr/>
       </div>
     </React.Fragment>
   );
